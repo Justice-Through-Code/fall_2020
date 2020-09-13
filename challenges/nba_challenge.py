8fea7e20310e14df2b4cf5f401b0471662c05994
Mark Bunday <mabunday@amazon.com>
2020-09-13 12:55:51 -0700

    Add challenge file

---
+++ challenges/nba_challenge.py
@@ -0,0 +1,33 @@
+print("Challenge 2.1:")
+print("Create a variable for the number of 3pt shots made by Fred VanVleet")
+print("Create a variable for the number of 3 pt shots made by James Harden")
+jamal_murray_3pts_made = 46
+# TODO: Create variable here for number of 3 pt shots made by Fred VanVleet
+# TODO: Create variable here for number of 3 pt shots made by James Harden
+print()
+
+print("Challenge 2.2")
+print("Print the variables that track the number of 3 pt shots made by each of the three players")
+print(f"In the 2020 NBA playoffs, Jamal Murray made {jamal_murray_3pts_made} 3 point shots")
+# TODO: Create print statement here for Fred VanVleet
+# TODO: Create print statement here for James Harden
+print()
+
+print("Challenge 2.3: Store the number of three point shot attempts in variables for each player")
+# TODO: Create variable here for number of 3 pt shot attempts by Jamal Murray
+# TODO: Create variable here for number of 3 pt shot attempts by Fred VanVleet
+# TODO: Create variable here for number of 3 pt shot attempts by James Harden
+print()
+
+print("Challenge 2.4: Build on your print statement")
+# TODO: Copy the three print statements you wrote in Challenge 2.2 and extend them to also print
+# the number of three point shots for each player. E.g., output should be similar to
+# "In the 2020 NBA playoffs, player X made Y 3 point shots and Z 3 point shot attempts."
+print()
+
+print("Challenge 2.5: Calculate and print the three point percentage for each player")
+# TODO: Calculate the three point percentage, which is given by `three points made/three point attempts`
+# TODO: Calculate and print the 3 point percentage for Jamal Murray
+# TODO: Calculate and print the 3 point percentage for Fred VanVleet
+# TODO: Calculate and print the 3 point percentage for James Harden
+
